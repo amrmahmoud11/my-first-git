7c6ad2404f5cc44575197b83c87361d6361c6c7a
Amr Mahmoud <amrmahmoud@aucegypt.edu>
2018-05-15 22:57:18 +0200

    i added test runner target

---
+++ src/test/java/seleniumProject/testrunnerTest.java
@@ -0,0 +1,12 @@
+package seleniumProject;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith	(Cucumber.class)
+@CucumberOptions(tags= {"@tag"})
+public class testrunnerTest {
+
+}
